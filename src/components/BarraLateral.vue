c0d11c4a3ca2017afd3c21fd2bfc39cb3544dfcf
alan.cleveston <alan.cleveston@softagon.com.br>
2021-11-03 13:02:53 -0300

    .

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,30 +1,52 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/logo.png" alt="">
-        </h1>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+  </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'BarraLateral'
+  name: 'BarraLateral',
+  emits: ['aoTemaAlterado'],
+  data() {
+    return {
+      modoEscutoAtivo: false,
+    }
+  },
+  computed: {
+    textoBotao () {
+      if (this.modoEscutoAtivo) {
+        return 'Desativar modo escuro'
+      }
+      return 'Ativar modo escuro'
+    }
+  },
+  methods: {
+    alterarTema() {
+        this.modoEscutoAtivo = !this.modoEscutoAtivo
+        this.$emit('aoTemaAlterado', this.modoEscutoAtivo)
+    },
+  },
 })
 </script>
 
 <style scoped>
 header {
-    padding: 1rem;
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
-    header {
-        padding: 2.5rem;
-        height: auto;
-    }
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
 }
 </style>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="is-flex is-align-items-center is-justify-content-space-between">
+  <section
+    class="is-flex is-align-items-center is-justify-content-space-between"
+  >
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
-      <span>Play</span>
-    </button>
-    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
-      <span>Stop</span>
-    </button>
-  </div>
+    <Botao
+      @clicado="iniciar"
+      icone="fas fa-play"
+      texto="play"
+      :desabilitado="cronometroRodando"
+    />
+    <Botao
+      @clicado="finalizar"
+      icone="fas fa-stop"
+      texto="stop"
+      :desabilitado="!cronometroRodando"
+    />
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Cronometro from './Cronometro.vue'
+import Botao from './Botao.vue'
 
 export default defineComponent({
   nome: 'Temporizador',
   emits: ['aoTemporizadorFinalizado'],
   components: {
     Cronometro,
+    Botao,
   },
   data() {
     return {
